ab5c55aadede7dbdfacf8d6b19718494c0871b23
pizzawarrior <i.norstad@gmail.com>
2023-11-08 17:38:15 -0800

    lightweight CRUD app fully functional

---
+++ backend/database.py
@@ -0,0 +1,42 @@
+from model import Todo
+# MongoDB driver:
+import motor.motor_asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+my_id = os.getenv("CONNECTION_STRING")
+
+client = motor.motor_asyncio.AsyncIOMotorClient({my_id})
+database = client.get_database("TodoList")
+collection = database.get_collection("todo")
+
+# Get one todo:
+async def fetch_one_todo(title):
+    document = await collection.find_one({"title":title})
+    return document
+
+# Get list of todos:
+async def fetch_all_todos():
+    todos = []
+    cursor = collection.find({})
+    async for document in cursor:
+        todos.append(Todo(**document))
+    return todos
+
+async def create_todo(todo):
+    document = todo
+    result = await collection.insert_one(document)
+    return document
+
+async def change_todo(title, desc):
+    # find the todo by title, and we will update the description using $set
+    await collection.update_one({"title":title}, {"$set":{
+        "description":desc}})
+    document = await collection.find_one({"title":title})
+    return document
+
+async def remove_todo(title):
+    await collection.delete_one({"title":title})
+    return True
---
+++ backend/main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from model import Todo
+
+# app object
+app = FastAPI()
+
+from database import (
+    fetch_one_todo,
+    fetch_all_todos,
+    create_todo,
+    change_todo,
+    remove_todo,
+)
+
+origins = ['https://localhost:3000']
+
+# obligatory CORS handling
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# test
+@app.get("/")
+def read_root():
+    return {"ping": "pong"}
+
+# get list of todos
+@app.get("/api/todo")
+async def get_todo():
+    response = await fetch_all_todos()
+    return response
+
+# get a single todo by title
+@app.get("/api/todo/{title}", response_model=Todo)
+async def get_one(title):
+    response = await fetch_one_todo(title)
+    if response:
+        return response
+    raise HTTPException(404, f"there is no todo item with the title {title}")
+
+# create a todo
+@app.post("/api/todo", response_model=Todo)
+async def post_todo(todo:Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, "Request could not be completed")
+
+# update a single todo
+@app.put("/api/todo{title}", response_model=Todo)
+async def update_todo(title:str, desc:str):
+    response = await change_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(404, f'there is no todo with the title {title}')
+
+# delete a todo
+@app.delete("/api/todo{title}")
+async def delete_todo(title):
+    response = await remove_todo(title)
+    if response:
+        return 'deletion was successful'
+    raise HTTPException(404, f'there is no todo with the title {title}')
